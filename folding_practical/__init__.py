017f6f7d994e3f57de6a3b1fb74c706df2c3c8b1
ben-shin <77782287+ben-shin@users.noreply.github.com>
2026-07-30 15:16:52 +0100

    Update __init__.py

--- folding_practical/__init__.py
+++ folding_practical/__init__.py
@@ -1,16 +1,18 @@
-"""tools for importing organizing fitting and plotting GFP denaturation data."""
+"""Tools for importing, organizing, fitting, and plotting GFP denaturation data."""
 
 from .models import FitResult, fit_four_parameter_logistic, fit_two_state_denaturation
 from .plate_io import load_plate_csv, load_plate_csvs
-from .project import GroupAssignment, build_group_dataframe
+from .project import GroupAssignment, build_group_dataframe, build_spectrum_dataframe
 
 __all__ = [
-  "FitResult",
-  "GroupAssignment",
-  "build_group_dataframe",
-  "fit_four_parameter_logistic",
-  "load_plate_csv",
-  "load_plate_csvs"
+    "FitResult",
+    "GroupAssignment",
+    "build_group_dataframe",
+    "build_spectrum_dataframe",
+    "fit_four_parameter_logistic",
+    "fit_two_state_denaturation",
+    "load_plate_csv",
+    "load_plate_csvs",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.2"
