695ae51c44600f146dceb5af2a67e3f899c7fee2
ben-shin <77782287+ben-shin@users.noreply.github.com>
2026-07-30 14:25:08 +0100

    Add imports for model fitting functions in tests

---
+++ test_models.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from folding_practical.models import (
+  fit_four_parameter_logistic,
+  fit_two_state_denaturation,
+  four_parameter_logistic,
+  two_state_denaturation_signal,
+)
